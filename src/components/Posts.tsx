7ebf3f27b1acc64661910cfae0ce8d012e82e572
Gary Sun <garysun0451@gmail.com>
2023-07-13 20:21:20 +1000

    Implement pagination

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,6 +1,7 @@
 import 'react';
 import { Post } from '../global';
 import styles from "./Posts.module.css";
+import { useState } from 'react';
 
 type Props = {
   id: string;
@@ -8,30 +9,58 @@ type Props = {
 }
 
 export default function PostBlock({ id, posts }: Props) {
+  const PAGE_SIZE = 4;
+  const [page, setPage] = useState(0);
 
   const sortedPosts = posts
     .map(({ date, ...p }) => { return { date: new Date(date), ...p } })
     .sort((a, b) => b.date.getTime() - a.date.getTime());
 
+  const maxPage = Math.floor(sortedPosts.length / PAGE_SIZE);
+  const pageIndexes = Array.from({ length: maxPage + 1 }, (_, i) => (i));
+
+  const startIndex = page * PAGE_SIZE;
+  const currPosts = sortedPosts.slice(startIndex, startIndex + PAGE_SIZE);
+
+  const toDiv = (path: string, date: Date, title: string, description: string) => {
+    return <a href={path} key={path}>
+      <div className={styles.post}>
+        <div className={styles.date}>
+          {date.toLocaleDateString("en-gb", {
+            day: 'numeric', month: 'short', year: 'numeric'
+          })}
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.description}>{description}</div>
+        </div>
+      </div>
+    </a>
+  };
+
   return (
     <div id={id} className={styles.posts}>
       <hr />
-      {sortedPosts.map(({ path, date, title, description }) => (
-        <a href={path} key={path}>
-          <div className={styles.post}>
-            <div className={styles.date}>
-              {date.toLocaleDateString("en-gb", {
-                day: 'numeric', month: 'short', year: 'numeric'
-              })}
-            </div>
-            <div className={styles.content}>
-              <div className={styles.title}>{title}</div>
-              <div className={styles.description}>{description}</div>
-            </div>
-          </div>
-        </a>
-      ))
-      }
+      {currPosts.map(({ path, date, title, description }) => toDiv(path, date, title, description))}
+      <div className={styles.pagination}>
+        <button onClick={() => setPage(0)} disabled={page === 0}>
+          «
+        </button>
+        <button onClick={() => setPage(page - 1)} disabled={page === 0}>
+          ‹
+        </button>
+        {pageIndexes.map((n, i) =>
+          <button key={i} onClick={() => setPage(n)} style={{ fontWeight: n === page ? 'bold' : 'normal' }}>
+            {n}
+          </button>
+        )}
+        <button onClick={() => setPage(page + 1)} disabled={page >= maxPage}>
+          ›
+        </button>
+        <button onClick={() => setPage(maxPage)} disabled={page >= maxPage}>
+          »
+        </button>
+      </div>
     </div >
   )
 }
